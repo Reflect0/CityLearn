e2d818d96e7aef7db5c56e03117b66bb1143fe5d
Aisling Pigott <aipi0122@colorado.edu>
2021-02-08 15:04:05 -0700

    working towards decoupled building types and models, also bug with pandapower power flows and PV generators

--- reward_function.py
+++ reward_function.py
@@ -1,5 +1,5 @@
 """
-This function is intended to wrap the rewards returned by the CityLearn RL environment, and is meant to 
+This function is intended to wrap the rewards returned by the CityLearn RL environment, and is meant to
 be modified at will.
 This reward_function takes all the electrical demands of all the buildings and turns them into one or multiple rewards for the agent(s)
 
@@ -16,27 +16,22 @@ class reward_function_ma:
         self.building_info = building_info
 
     # Electricity_demand contains negative values when the building consumes more electricity than it generates
-    def get_rewards(self, electricity_demand):  
+    def get_rewards(self, electricity_demand):
+        # original rewards function from citylearn
         electricity_demand = np.float32(electricity_demand)
         total_electricity_demand = 0
         for e in electricity_demand:
             total_electricity_demand += -e
-            
+
         electricity_demand = np.array(electricity_demand)
-        
+
         return list(np.sign(electricity_demand)*0.01*(np.array(np.abs(electricity_demand))**2 * max(0, total_electricity_demand)))
-        
-        # Single-agent reward
-        # reward_ = np.array(electricity_demand)**3.0
-        # reward_[reward_>0] = 0
-        # return list(reward_)
-    
-      
+
 # Reward function for the single-agent (centralized) agent
 def reward_function_sa(electricity_demand):
 
     reward_ = -np.array(electricity_demand).sum()
     reward_ = max(0, reward_)
     reward_ = reward_**3.0
-    
+
     return reward_
