637970ae755aa153882ac29e537328e1f0dee69c
apigott <aisling.pigott@colorado.edu>
2021-03-04 21:33:32 -0700

    for batch runs

---
+++ run_slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --nodes=1  #Allocate whatever you need here
+#SBATCH --cpus-per-task=4  #Allocate whatever you need here
+#SBATCH --output=run.out
+#SBATCH --job-name=test
+#SBATCH --time=0-12:00:00
+#SBATCH --mail-user=aipi0122@colorado.edu
+#SBATCH --mail-type=ALL
+
+module purge
+source /curc/sw/anaconda3/2019.07/bin/activate
+conda activate cities
+python -u rl_main.py
