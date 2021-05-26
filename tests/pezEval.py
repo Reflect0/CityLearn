7d6cbd54987ef8aca1158bc3f0f2ce418460f6f5
apigott <aisling.pigott@colorado.edu>
2021-05-26 11:53:31 -0600

    test eval on slurm env

---
+++ tests/pezEval.py
@@ -0,0 +1,80 @@
+import multiprocessing
+import sys
+from pettingzoo.test import parallel_api_test
+
+import multiprocessing
+import sys
+from pettingzoo.test import parallel_api_test
+from citylearn import GridLearn
+from citylearn import MyEnv
+from pathlib import Path
+from stable_baselines3.ppo import MlpPolicy
+from stable_baselines3 import PPO
+import gym
+import numpy as np
+
+import multiprocessing
+import sys
+import supersuit as ss
+
+import time
+
+# multiprocessing.set_start_method("fork")
+
+climate_zone = 1
+data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
+buildings_states_actions = '../citylearn/buildings_state_action_space.json'
+
+config = {
+    "data_path":data_path,
+    "climate_zone":climate_zone,
+    "buildings_states_actions_file":buildings_states_actions,
+    "hourly_timesteps":4
+}
+
+grid = GridLearn(**config)
+
+envs = [MyEnv(grid), MyEnv(grid)]
+
+print('padding action/observation spaces...')
+envs = [ss.pad_action_space_v0(env) for env in envs]
+envs = [ss.pad_observations_v0(env) for env in envs]
+
+print('creating pettingzoo env...')
+envs = [ss.pettingzoo_env_to_vec_env_v0(env) for env in envs]
+
+print('stacking vec env...')
+nenvs = 2
+envs = [ss.concat_vec_envs_v0(env, nenvs, num_cpus=1, base_class='stable_baselines3') for env in envs]
+
+from copy import deepcopy
+grid2 = deepcopy(grid)
+
+grids = [grid, grid2]
+
+print('setting the grid...')
+for env in envs:
+    for n in range(nenvs):
+        env.venv.vec_envs[n].par_env.aec_env.env.env.env.env.grid = grids[n]
+
+models = [PPO.load(f"model50_{m}") for m in range(len(envs))]
+
+obss = [env.reset() for env in envs]
+for ts in range(5): # test on 5 timesteps
+    for m in range(len(models)): # again, alternate through models
+
+        # get the current observation from the perspective of the active team
+        # this can probably be cleaned up
+        foo = []
+        for e in range(nenvs):
+            bar = list(envs[m].venv.vec_envs[n].par_env.aec_env.env.env.env.env.state().values())
+            for item in bar:
+                while len(item) < 29:
+                    item = np.append(item, 0)
+            foo += bar
+
+        foo = np.vstack(foo)
+        obss[m] = np.vstack(foo)
+
+        action = models[m].predict(obss[m])[0] # send it to the SB model to select an action
+        obss[m], reward, done, info = envs[m].step(action) # update environment
