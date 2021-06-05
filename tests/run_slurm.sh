405d80f77c2b444a6f23ffe2c7fa53c643cc9106
Aisling Pigott <aipi0122@login13.rc.int.colorado.edu>
2021-06-04 20:24:03 -0600

    run slurm

--- tests/run_slurm.sh
+++ tests/run_slurm.sh
@@ -3,13 +3,13 @@
 #SBATCH --nodes=1  #Allocate whatever you need here
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=2  #Allocate whatever you need here
-#SBATCH --output=rl25_c2_reducedss.out
+#SBATCH --output=10_houses_eval.out
 #SBATCH --job-name=test
-#SBATCH --time=0-06:00:00
+#SBATCH --time=0-12:00:00
 #SBATCH --mail-user=aipi0122@colorado.edu
 #SBATCH --mail-type=ALL
 
 module purge
 source /curc/sw/anaconda3/2019.07/bin/activate
 conda activate cities
-python -u pezTest.py
+python -u pezEval.py
