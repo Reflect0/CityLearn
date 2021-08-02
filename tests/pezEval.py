cd704d1c4a87bd2dc1938224747d177621753803
apigott <aisling.pigott@colorado.edu>
2021-08-02 14:18:22 -0600

    typo

--- tests/pezEval.py
+++ tests/pezEval.py
@@ -21,7 +21,7 @@ import time
 import os
 
 # multiprocessing.set_start_method("fork")
-model_name = "fixed_rbc_agent"
+model_name = "fixed_rbc_agents"
 
 climate_zone = 1
 data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
