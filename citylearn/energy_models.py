e9819594910b412ae89827a92621dbb59427d7bb
apigott <aisling.pigott@colorado.edu>
2021-08-05 09:14:33 -0600

    r1 = your voltage + your neighbors voltage, r2 = your voltage, r3 = rescaled

--- citylearn/energy_models.py
+++ citylearn/energy_models.py
@@ -111,7 +111,7 @@ class Building:
 
     def assign_bus(self, bus):
         self.bus = bus
-        self.buildingId += f'{bus:02}'
+        self.buildingId += f'{bus:03}'
 
     def set_attributes(self, file):
         with open(file) as json_file:
@@ -212,10 +212,10 @@ class Building:
 
     def get_reward(self, net): # dummy cost function
         #my_voltage_dev = (10*np.clip(net.res_bus.loc[self.bus]['vm_pu']-1,-.1,.1))**2
-        my_voltage_dev = (10*net.res_bus.loc[self.bus]['vm_pu']-1)**2
+        my_voltage_dev = (20*net.res_bus.loc[self.bus]['vm_pu']-1)**2
         my_cons = (self.current_gross_electricity_demand - self.net_elec_cons_mid) / self.net_elec_cons_range
-        #my_neighbors_voltage_dev = sum(np.square(10 * np.clip(net.res_bus.loc[self.neighbors]['vm_pu']-1,-.1,.1)))
-        reward = -1 * (my_voltage_dev)#+ 0.3*my_neighbors_voltage_dev) #+ my_neighbors_voltage_dev)
+        my_neighbors_voltage_dev = sum(np.square(10 * np.clip(net.res_bus.loc[self.neighbors]['vm_pu']-1,-.1,.1)))
+        reward = -1 * (my_voltage_dev + 0.3*my_neighbors_voltage_dev) #+ my_neighbors_voltage_dev)
         #print(reward)
         reward = (reward + 365)/9
         # if not self.rbc:
