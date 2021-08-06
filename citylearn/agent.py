05e30c123b0f61779dd8d34e595962d12e90c76d
apigott <aisling.pigott@colorado.edu>
2021-08-06 11:24:30 -0600

    typo

--- citylearn/agent.py
+++ citylearn/agent.py
@@ -112,6 +112,19 @@ class RBC_Agent_v2:
     def reset_action_tracker(self):
         self.action_tracker = []
 
+    def get_tou_storage(self, hour):
+        if hour < 7:
+            a = 0.1383
+        elif hour < 16:
+            a = -0.05
+        elif hour < 18:
+            a = -0.11
+        elif hour < 22:
+            a = -0.06
+        else:
+            a = 0.085
+        return a
+
     def predict(self):
         hour_day = self.env.time_step/self.env.hourly_timesteps % 24
         tou_storage = self.get_tou_storage(hour_day)
