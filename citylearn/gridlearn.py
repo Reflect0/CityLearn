bc301a66c0d1da1bcdaa6c84cc145afd1531e8e0
Aisling Pigott <aipi0122@login12.rc.int.colorado.edu>
2021-08-10 17:30:12 -0600

    junk

--- citylearn/gridlearn.py
+++ citylearn/gridlearn.py
@@ -104,9 +104,9 @@ class GridLearn: # not a super class of the CityLearn environment
 
         net.ext_grid.at[0,'vm_pu'] = 1.0
 
-        # pp.create_shunt_as_capacitor(net,14,0.3,0)
-        # pp.create_shunt_as_capacitor(net,24,0.6,0)
-        # pp.create_shunt_as_capacitor(net,30,1.2,0)
+        pp.create_shunt_as_capacitor(net,14,0.3,0)
+        pp.create_shunt_as_capacitor(net,24,0.6,0)
+        pp.create_shunt_as_capacitor(net,30,1.2,0)
         return net
 
     def add_houses(self, n, pv_penetration):
