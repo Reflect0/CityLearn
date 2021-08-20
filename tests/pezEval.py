aad7ddd82813ceb8fc6600f33be55ea18626098a
apigott <aisling.pigott@colorado.edu>
2021-08-19 21:15:59 -0600

    much higher learning rate

--- tests/pezEval.py
+++ tests/pezEval.py
@@ -20,7 +20,7 @@ import time
 import os
 
 # multiprocessing.set_start_method("fork")
-model_name = "low_ent"
+model_name = "high_lr"
 
 climate_zone = 1
 data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
