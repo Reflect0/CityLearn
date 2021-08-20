975e58d76e73d0e2083012a1fad0b7b8cc7e8a06
apigott <aisling.pigott@colorado.edu>
2021-08-19 21:27:36 -0600

    rescale reward, low ent

--- tests/pezEval.py
+++ tests/pezEval.py
@@ -20,7 +20,7 @@ import time
 import os
 
 # multiprocessing.set_start_method("fork")
-model_name = "rescale_rew_high_lr"
+model_name = "rescale_rew_low_ent"
 
 climate_zone = 1
 data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
