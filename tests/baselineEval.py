a42f4a093b8b8c09deb875b9d9da82bf10482d78
apigott <aisling.pigott@colorado.edu>
2021-08-20 23:46:47 -0600

    3 wk

--- tests/baselineEval.py
+++ tests/baselineEval.py
@@ -25,7 +25,7 @@ data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
 buildings_states_actions = '../citylearn/buildings_state_action_space.json'
 
 config = {
-    "model_name":"to_scale",
+    "model_name":"min_p_ext_3wk_baseline",
     "data_path":data_path,
     "climate_zone":climate_zone,
     "buildings_states_actions_file":buildings_states_actions,
@@ -51,7 +51,7 @@ for env in envs:
 
 sum_reward = 0
 obss = [env.reset() for env in envs]
-for ts in range(1*7*24*4): # test on 5 timesteps
+for ts in range(3*7*24*4): # test on 5 timesteps
     for m in range(len(envs)): # again, alternate through models
 
         obss[m], reward, done, info = envs[m].step({}) # update environment
