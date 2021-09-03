121f2052bc780f3cd148468c7c581b3aa7605e6a
apigott <aisling.pigott@colorado.edu>
2021-09-02 22:57:19 -0600

    baseline 50% rl

--- tests/baselineEval.py
+++ tests/baselineEval.py
@@ -30,7 +30,7 @@ config = {
     "climate_zone":climate_zone,
     "buildings_states_actions_file":buildings_states_actions,
     "hourly_timesteps":4,
-    "percent_rl":0.1,
+    "percent_rl":0.5,
     # "percent_rl":1,
     "nclusters":1,
     "max_num_houses":None
