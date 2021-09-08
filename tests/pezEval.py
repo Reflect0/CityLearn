f65260bafebde840b0697156e9b31d5722a632e3
apigott <aisling.pigott@colorado.edu>
2021-09-07 19:04:11 -0600

    eval 1x load

--- tests/pezEval.py
+++ tests/pezEval.py
@@ -20,7 +20,7 @@ import time
 import os
 
 # multiprocessing.set_start_method("fork")
-model_name = "1x_baseline"
+model_name = "1x_load"
 
 climate_zone = 1
 data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
