eb20153ab94ccb83b55701d16655edafa7a703e1
apigott <aisling.pigott@colorado.edu>
2021-09-08 12:03:39 -0600

    whole year

--- tests/baselineEval.py
+++ tests/baselineEval.py
@@ -25,7 +25,7 @@ data_path = Path("../citylearn/data/Climate_Zone_"+str(climate_zone))
 buildings_states_actions = '../citylearn/buildings_state_action_space.json'
 
 config = {
-    "model_name":"para_baseline",
+    "model_name":"yr_baseline",
     "data_path":data_path,
     "climate_zone":climate_zone,
     "buildings_states_actions_file":buildings_states_actions,
@@ -51,7 +51,7 @@ for env in envs:
 
 sum_reward = 0
 obss = [env.reset() for env in envs]
-for ts in range(20*24*4): # test on 5 timesteps
+for ts in range(365*24*4): # test on 5 timesteps
     for m in range(len(envs)): # again, alternate through models
         obss[m], reward, done, info = envs[m].step({}) # update environment
 
