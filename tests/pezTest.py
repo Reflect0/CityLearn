dcd5096a1730cb5785b70363918a1ccf635402b4
apigott <aisling.pigott@colorado.edu>
2021-09-13 12:23:41 -0600

    para with 90% RL

--- tests/pezTest.py
+++ tests/pezTest.py
@@ -16,7 +16,7 @@ import random
 random.seed(12)
 np.random.seed(12)
 
-model_name = "v5_80"
+model_name = "v5_90"
 
 tic = time.time()
 
@@ -30,7 +30,7 @@ config = {
     "climate_zone":climate_zone,
     "buildings_states_actions_file":buildings_states_actions,
     "hourly_timesteps":4,
-    "percent_rl":0.8,
+    "percent_rl":0.9,
     "nclusters":1,
     "max_num_houses":None
 }
